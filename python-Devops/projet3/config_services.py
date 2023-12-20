190770c50644c9d6eee9cd73dab763e93c13aff8
MarieAhmed <marieamandine.ahmed@kedgebs.com>
2023-12-20 14:47:29 +0100

    projet 3

---
+++ python-Devops/projet3/config_services.py
@@ -0,0 +1,10 @@
+import subprocess
+def install_apache():
+ subprocess.run(["sudo", "apt-get", "update"])
+ subprocess.run(["sudo", "apt-get", "install", "-y", "apache2"])
+
+def install_ssh():
+ subprocess.run(["sudo", "apt-get", "install", "-y", "openssh␂server"])
+if __name__ == "__main__":
+ install_apache()
+ install_ssh()
